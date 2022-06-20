29f26e28b6558076f8fa85eaebd5f74d8ece0aee
Felipe Sacramento <mzssacramento@gmail.com>
2022-06-20 13:01:38 -0300

    Alpha

---
+++ src/javalinkedlist/JavaMain.java
@@ -0,0 +1,15 @@
+
+package javalinkedlist;
+
+
+public class JavaMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FormSystem form = new FormSystem();
+        form.setVisible(true);
+    }
+    
+}
